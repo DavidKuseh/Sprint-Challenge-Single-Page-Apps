ab9d18a81297800015d741fdc5f6093d11c04872
David Kuseh <davegru203@gmail.com>
2019-09-20 08:12:29 +0000

    added state, get request and mapped over list of charcaters in CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
+    // debugger
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios.get("https://rick-api.herokuapp.com/api/character")
+     .then(res => {
+        setCharacter(res.data.results);
+     })
+     .catch(error =>{
+       console.log(error);
+     })
+  }, [id]);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        character.map(person => {
+          {person.name} {person.status} {person.species}
+        })
+      }
     </section>
   );
 }
